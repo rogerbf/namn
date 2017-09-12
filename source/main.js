b4da6f22f6d8ce07bf635a18f121e3a567206106
rogerbf <roger@bergfalk.nu>
2017-09-12 23:27:05 +0200

    search functionality

--- source/main.js
+++ source/main.js
@@ -1,1 +1,15 @@
-export default `namn`
+import fetch from "isomorphic-unfetch"
+import cheerio from "cheerio"
+
+const getSearchUrl = name =>
+  `http://www.scb.se/hitta-statistik/sverige-i-siffror/namnsok/Search/?nameSearchInput=${name}`
+
+const search = async name => {
+  const url = getSearchUrl(name)
+  const response = await fetch(url)
+  const html = await response.text()
+  const $ = cheerio.load(html)
+  return $(`#name-result`).text()
+}
+
+export default search
