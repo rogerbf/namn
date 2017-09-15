7d31dfc193886729458e5bef80b46d07dbcb7850
rogerbf <roger@bergfalk.nu>
2017-09-15 19:49:55 +0200

    parse the total number of people

--- source/main.js
+++ source/main.js
@@ -4,12 +4,22 @@ import cheerio from "cheerio"
 const getSearchUrl = name =>
   `http://www.scb.se/hitta-statistik/sverige-i-siffror/namnsok/Search/?nameSearchInput=${name}`
 
-const search = async name => {
+const getTotal = $ =>
+  parseInt(
+    $(`h2`, `.nameSearchResultNumbers`)
+      .text()
+      .trim()
+      .replace(/\D/g, ``),
+    10
+  )
+
+const search = async name => {
   const url = getSearchUrl(name)
   const response = await fetch(url)
   const html = await response.text()
   const $ = cheerio.load(html)
-  return $(`#name-result`).text()
+  const total = getTotal($)
+  return total
 }
 
 export default search
