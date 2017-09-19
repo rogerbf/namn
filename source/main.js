2cfc255fa9c7a9c275dcacba911d39d808a3cb6f
rogerbf <roger@bergfalk.nu>
2017-09-19 23:05:06 +0200

    skeleton solution for consolidating statistics

--- source/main.js
+++ source/main.js
@@ -13,13 +13,35 @@ const getTotal = $ =>
     10
   )
 
+const getResultStrings = $ =>
+  $(`p`, `.nameSearchResultNumbers`)
+    .toArray()
+    .map(element =>
+      $(element)
+        .text()
+        .trim()
+    )
+
+const getNumbers = (sex, strings) => {
+  const filteredStrings = strings.filter(s => new RegExp(sex).test(s))
+  const firstName = parseInt(
+    filteredStrings.filter(s => /tilltalsnamn/.test(s)).pop()
+  )
+  return { firstName }
+}
+
 const search = async name => {
   const url = getSearchUrl(name)
   const response = await fetch(url)
   const html = await response.text()
   const $ = cheerio.load(html)
   const total = getTotal($)
-  return total
+  const resultStrings = getResultStrings($)
+  const women = getNumbers(`kvinnor`, resultStrings)
+  return {
+    total,
+    women
+  }
 }
 
 export default search
